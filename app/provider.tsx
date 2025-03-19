e87d85ce976c300d4bea3d45ab89af34c2ddae10
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-03-19 11:06:10 +0530

    User to DB

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,16 +1,20 @@
 "use client"
 import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { Suspense } from "react";
+import AuthProvider from "./AuthProvider";
 
 const Provider = ({ children }) => {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
-  
+
     return (
-    <div>
-<ConvexProvider client={convex}>
-{children}
-</ConvexProvider>
-    </div>
-  )
+        <Suspense fallback={<div>Loading...</div>}>
+            <ConvexProvider client={convex}>
+                <AuthProvider>
+                    {children}
+                </AuthProvider>
+            </ConvexProvider>
+        </Suspense>
+    )
 }
 
 export default Provider
